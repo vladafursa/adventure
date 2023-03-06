e9617b1e1b6698ba2587efab3c7875d484d29970
vladafursa <tyanka33@mail.ru>
2023-03-06 00:48:48 +0000

    Initial Commit

---
+++ adventure game/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  adventure game
+//
+//  Created by Влада Фурса on 6.03.23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
